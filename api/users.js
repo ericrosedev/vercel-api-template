a5be1fee8224b72d26f0a9a285021cae96cd6a6a
Eric Rose <eric@ericrose.dev>
2023-07-11 23:22:58 -0700

    testing

--- api/users.js
+++ api/users.js
@@ -1,8 +1,9 @@
 module.exports = (req, res) => {
     if (req.method === 'GET'){
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.setHeader('content-type', 'application/json; charset=utf-8')
         res.json([
             {name: "Eric", location: "Sacramento, CA"},
             {name: "Dakota", location: "Sacramento, CA"},
