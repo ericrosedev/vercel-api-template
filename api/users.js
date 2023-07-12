b2c27b909aa8ea8cf4d26da8c27bd33086a906c2
Eric Rose <eric@ericrose.dev>
2023-07-11 23:48:06 -0700

    testing

--- api/users.js
+++ api/users.js
@@ -1,4 +1,31 @@
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  
+    res.setHeader('Access-Control-Allow-Origin', '*')
+  
+    // another common pattern
+  
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  
+    res.setHeader(
+  
+      'Access-Control-Allow-Headers',
+  
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  
+    )
+  
+    if (req.method === 'OPTIONS') {
+  
+      res.status(200).end()
+  
+      return
+  
+    }
+
     if (req.method === 'GET'){
         res.json([
             {name: "Eric", location: "Sacramento, CA"},
@@ -7,7 +34,39 @@ module.exports = (req, res) => {
             {name: "Kevin", location: "Folsom, CA"},
             {name: "Melinda", location: "Folsom, CA"}
         ])
-    } else {
-
     }
-}
+  
+    return await fn(req, res)
+  
+  }
+  
+  
+  const handler = (req, res) => {
+  
+    const d = new Date()
+  
+    res.end(d.toString())
+  
+  }
+  
+  
+  module.exports = allowCors(handler)
+
+
+
+
+
+
+//   module.exports = (req, res) => {
+//     if (req.method === 'GET'){
+//         res.json([
+//             {name: "Eric", location: "Sacramento, CA"},
+//             {name: "Dakota", location: "Sacramento, CA"},
+//             {name: "Amy", location: "Far Away"},
+//             {name: "Kevin", location: "Folsom, CA"},
+//             {name: "Melinda", location: "Folsom, CA"}
+//         ])
+//     } else {
+
+//     }
+// }
