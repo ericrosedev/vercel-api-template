dc416c6625c48280dd8530e09aa9f444b811f5b6
Eric Rose <eric@ericrose.dev>
2023-07-11 23:16:55 -0700

    allow CORS

--- api/users.js
+++ api/users.js
@@ -1,5 +1,8 @@
 module.exports = (req, res) => {
     if (req.method === 'GET'){
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.json([
             {name: "Eric", location: "Sacramento, CA"},
             {name: "Dakota", location: "Sacramento, CA"},
