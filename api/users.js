05109a2bf9f55b34584a65d6c867d76a4234d64a
Eric Rose <eric@ericrose.dev>
2023-07-11 23:26:37 -0700

    testing

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     if (req.method === 'GET'){
-        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Origin', 'https://rainbow-horse-a069c1.netlify.app/')
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
         res.setHeader('content-type', 'application/json; charset=utf-8')
