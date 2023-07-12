896d25255096a41cc07a9cfc4fbcccc622351e75
Eric Rose <eric@ericrose.dev>
2023-07-11 23:50:26 -0700

    testing

--- api/users.js
+++ api/users.js
@@ -51,22 +51,3 @@ const allowCors = fn => async (req, res) => {
   
   
   module.exports = allowCors(handler)
-
-
-
-
-
-
-//   module.exports = (req, res) => {
-//     if (req.method === 'GET'){
-//         res.json([
-//             {name: "Eric", location: "Sacramento, CA"},
-//             {name: "Dakota", location: "Sacramento, CA"},
-//             {name: "Amy", location: "Far Away"},
-//             {name: "Kevin", location: "Folsom, CA"},
-//             {name: "Melinda", location: "Folsom, CA"}
-//         ])
-//     } else {
-
-//     }
-// }
