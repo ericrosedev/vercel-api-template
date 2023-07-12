7601150f9800bd76dc5e8f60e06ebb2e4b5bbb93
Eric Rose <eric@ericrose.dev>
2023-07-11 23:59:16 -0700

    testing

--- api/users.js
+++ api/users.js
@@ -1,55 +1,35 @@
-const allowCors = fn => async (req, res) => {
-
-    res.setHeader('Access-Control-Allow-Credentials', true)
-  
-    res.setHeader('Access-Control-Allow-Origin', 'https://rainbow-horse-a069c1.netlify.app')
-  
-    // another common pattern
-  
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', 'https://rainbow-horse-a069c1.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader(
-  
       'Access-Control-Allow-Headers',
-  
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  
-    )
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, hx-current-url'
+    );
   
     if (req.method === 'OPTIONS') {
-  
-      res.status(200).end()
-  
-      return
-  
-    }
-
-    if (req.method === 'GET'){
-        res.json([
-            {name: "Eric", location: "Sacramento, CA"},
-            {name: "Dakota", location: "Sacramento, CA"},
-            {name: "Amy", location: "Far Away"},
-            {name: "Kevin", location: "Folsom, CA"},
-            {name: "Melinda", location: "Folsom, CA"}
-        ])
-
-        return
+      res.status(200).end();
+      return;
     }
   
-    return await fn(req, res)
+    if (req.method === 'GET') {
+      res.json([
+        { name: 'Eric', location: 'Sacramento, CA' },
+        { name: 'Dakota', location: 'Sacramento, CA' },
+        { name: 'Amy', location: 'Far Away' },
+        { name: 'Kevin', location: 'Folsom, CA' },
+        { name: 'Melinda', location: 'Folsom, CA' },
+      ]);
+      return;
+    }
   
-  }
-  
+    return await fn(req, res);
+  };
   
   const handler = (req, res) => {
+    const d = new Date();
+    res.end(d.toString());
+  };
   
-    const d = new Date()
+  module.exports = allowCors(handler);
   
-    res.end(d.toString())
-  
-  }
-  
-  
-  module.exports = allowCors(handler)
